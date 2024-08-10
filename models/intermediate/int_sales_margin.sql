dc4665a46cb37e5152f18d09cbd51374fe1855bd
Alvaro Guerrero <alvaroguerrerosuarez.88@gmail.com>
2024-08-10 21:51:34 +0000

    commit ch4.3

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,8 +1,8 @@
 select
-    *,
     products_id,
-    (CAST(purchase_price as float64) * quantity) as purchase_cost,
-    ((revenue - (CAST(purchase_price as float64) * quantity)) / revenue) as margin
+    SUM(CAST(purchase_price as float64) * quantity) as purchase_cost,
+    (SUM(revenue) - SUM(CAST(purchase_price AS FLOAT64) * quantity)) / NULLIF(SUM(revenue), 0) AS margin
 from {{ref("stg_raw__sales")}}
 left join {{ref("stg_raw__product")}}
 using (products_id)
+group by products_id
