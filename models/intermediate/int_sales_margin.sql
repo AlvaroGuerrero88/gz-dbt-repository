dd1b4316a9a357aa4879eacaf2168ab2212cec14
Alvaro Guerrero <alvaroguerrerosuarez.88@gmail.com>
2024-08-10 20:53:26 +0000

    challenge 4.1

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,7 @@
+select
+    products_id,
+    (CAST(purchase_price as float64) * quantity) as purchase_cost,
+    ((revenue) - (CAST(purchase_price as float64) * quantity) / revenue) as margin
+from {{ref("stg_raw__sales")}}
+left join {{ref("stg_raw__product")}}
+using (products_id)
