74357c80039210ba2f4d879a007f9eecaa65bcef
Alvaro Guerrero <alvaroguerrerosuarez.88@gmail.com>
2024-08-14 00:35:50 +0000

    4.3

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,13 +1,11 @@
 select
     orders_id,
-    max(date_date) as date_date,
-    sum(revenue) as total_revenue,
-    sum(quantity) as total_quantity,
-    sum(CAST(purchase_price as float64) * quantity) as purchase_cost,
-    (SUM(revenue) - SUM(CAST(purchase_price AS FLOAT64) * quantity)) / NULLIF(SUM(revenue), 0) AS margin
-from {{ref("stg_raw__sales")}}
-left join {{ref("stg_raw__product")}}
-using (products_id)
-group by orders_id
+    date_date,
+    round(sum(revenue),2) as total_revenue,
+    round(sum(quantity),2) as total_quantity,
+    round(sum(purchase_cost),2) as purchase_cost,
+    round(sum(margin),2) AS margin
+from {{ref("int_sales_margin")}}
+group by orders_id, date_date
 order by orders_id desc
 
